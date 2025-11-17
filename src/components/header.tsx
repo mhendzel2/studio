71a06f0f67a3181b87eb6ee14e703d59392eaa18
Michael <mhendzellab@gmail.com>
2025-11-17 18:50:31 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
 import { useAppState } from '@/hooks/use-app-state';
+import Link from 'next/link';
 
 export function Header() {
   const { resetState } = useAppState();
@@ -10,12 +11,15 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <div className="mr-4 flex">
-          <a className="mr-6 flex items-center space-x-2" href="/">
+          <Link
+            href="/"
+            className="mr-6 flex items-center space-x-2"
+          >
             <Icons.logo className="h-6 w-6 text-primary" />
             <span className="font-bold sm:inline-block">
               FluoroSegStat
             </span>
-          </a>
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-2">
             <Button onClick={resetState} variant="outline">Reset</Button>
